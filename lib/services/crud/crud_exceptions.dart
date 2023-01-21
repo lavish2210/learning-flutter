dfe1c7e29717251958d10893666e21c0b20eeb40
Lavish Sachdeva <b20113@students.iitmandi.ac.in>
2023-01-21 11:42:18 +0530

    Protected Notes

--- lib/services/crud/crud_exceptions.dart
+++ lib/services/crud/crud_exceptions.dart
@@ -15,3 +15,5 @@ class CouldNotDeleteNote implements Exception {}
 class CouldNotFindNote implements Exception {}
 
 class CouldNotUpdateNote implements Exception {}
+
+class UserShouldBeSetBeforeReadingNotes implements Exception {}
